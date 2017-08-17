847c94c50e6497f60adcf8221c890023afc5d14b
patricewhite <patricewhite2012@gmail.com>
2017-08-17 16:11:06 -0400

    smoketest

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,5 +27,7 @@ print(os.listdir(settings.STATIC_ROOT))
 print(settings.MEDIA_ROOT)
 print(os.listdir(settings.MEDIA_ROOT))
 
+smoketest
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
