a7dfa222fbc490d9613f098b39b441d294f7e624
patricewhite <patricewhite2012@gmail.com>
2017-08-17 16:19:38 -0400

    trying stuff out

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import url, static
 from . import views
+import os.path
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -8,3 +9,16 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
